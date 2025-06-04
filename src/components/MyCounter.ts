4210da0c2ee528ccc6721c33eb476294594f2ed9
Anthony Fu <github@antfu.me>
2025-06-04 15:06:05 +0900

    fix: defineCustomElement

--- src/components/MyCounter.ts
+++ src/components/MyCounter.ts
@@ -2,10 +2,12 @@ import { defineCustomElement } from 'vue'
 import css from '../.generated/css'
 import Component from './MyCounter.vue'
 
-export const MyCounter = defineCustomElement({
-  ...Component,
-  shadowRoot: true,
-  styles: [css],
-} as any)
+export const MyCounter = defineCustomElement(
+  Component,
+  {
+    shadowRoot: true,
+    styles: [css],
+  },
+)
 
 customElements.define('my-counter', MyCounter)
