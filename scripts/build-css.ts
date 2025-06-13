34f7cb017fa19cb53fe93b546f617932ce50145e
coutinho <609127295@qq.com>
2025-06-13 18:03:12 +0800

    feat: watch css

--- scripts/build-css.ts
+++ scripts/build-css.ts
@@ -3,7 +3,6 @@ import fs from 'node:fs/promises'
 import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import c from 'ansis'
-import chokidar from 'chokidar'
 import { resolveModulePath } from 'exsolve'
 import { transform } from 'lightningcss'
 import { glob } from 'tinyglobby'
@@ -13,7 +12,7 @@ import config from '../uno.config'
 const SRC_DIR = fileURLToPath(new URL('../src', import.meta.url))
 const GLOBS = ['components/**/*.vue']
 const USER_STYLE = join(SRC_DIR, 'style.css')
-const GENERATED_CSS = join(SRC_DIR, '.generated/css.ts')
+export const GENERATED_CSS = join(SRC_DIR, '.generated/css.ts')
 const MINIFY = true
 
 export async function buildCSS() {
@@ -71,14 +70,3 @@ export async function buildCSS() {
   }
 }
 
-export async function watchCSS() {
-  const watcher = chokidar.watch(GLOBS, {
-    cwd: SRC_DIR,
-  })
-
-  watcher.on('change', async () => {
-    await buildCSS()
-  })
-
-  return watcher
-}
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from 'tsdown'
 import Vue from 'unplugin-vue/rolldown'
-import { buildCSS } from './scripts/build-css'
-
-await buildCSS()
+import { buildCSS,GENERATED_CSS } from './scripts/build-css'
 
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
   fixedExtension: true,
+  hooks: {
+    "build:prepare": async ({ options }) => {
+      await buildCSS();
+      let { watch, ignoreWatch = [] } = options;
+      if (!watch) return;
+      typeof ignoreWatch === "string" && (options.ignoreWatch = [ignoreWatch]);
+      Array.isArray(ignoreWatch) && ignoreWatch.push(GENERATED_CSS);
+      ignoreWatch = Array.from(new Set(ignoreWatch));
+    },
+  },
   plugins: [
     Vue(),
   ],
